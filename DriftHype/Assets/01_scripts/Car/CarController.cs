3322f6ec16a00adf6c04051079923fed26ef7709
wingyu0897 <wingyu0897@gmail.com>
2024-05-14 20:26:31 +0900

    car controller test

--- DriftHype/Assets/01_scripts/Car/CarController.cs
+++ DriftHype/Assets/01_scripts/Car/CarController.cs
@@ -4,49 +4,80 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    [Header("Value Setting")]
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float maxSpeed;
 	[SerializeField] private float acceleration;
+	[Range(0f, 180f)]
+	[SerializeField] private float accelAngle = 5f;
+
+	[Header("Turn")]
     [SerializeField] private float turnRate;
 
     [Header("References")]
     private Rigidbody rigid;
 
 	[Header("Flags")]
-	private float turnDesire;
+	private float angle;
+	private Vector3 velocity;
 
 	private void Awake()
 	{
 		rigid = GetComponent<Rigidbody>();
-	}
-
-	private void Update()
-	{
-		if (Input.GetKey(KeyCode.D))
-		{
-			Turn(1f);
-		}
-		else if (Input.GetKey(KeyCode.A))
-		{
-			Turn(-1f);
-		}
+		accelAngle *= Mathf.Deg2Rad;
 	}
 
 	private void FixedUpdate()
 	{
-		rigid.AddForce(transform.forward * moveSpeed * Time.fixedDeltaTime);
-		Vector3 velocity = rigid.velocity;
-		velocity.z = 0;
-		if (velocity.magnitude > maxSpeed)
-		{
-			velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
-			rigid.velocity = velocity;
-		}
+		Move();
 	}
 
-	private void Turn(float direction)
+	private void Move()
 	{
-		transform.Rotate(Vector3.up, direction * turnRate * Time.deltaTime);
+		velocity = Vector3.zero;
+
+		/*
+		 * 목표 방향 구하기
+		 * 실제 방향 = rigid.Velocity
+		 * 목표 방향과 실제 방향의 각도 차 구하기
+		 * 차가 *도 이하인가?
+		 * true) 가속
+		 * false) 방향 바꾸기
+		 */
+
+		Vector3 desireDir = transform.forward;
+		Vector3 velocityDir = rigid.velocity.normalized;
+		float angleDot = Vector3.Dot(desireDir, velocityDir);
+		if (rigid.velocity == Vector3.zero || (1 - Mathf.Abs(angleDot)) < accelAngle)
+		{
+			velocity = desireDir * CalculatedSpeed();
+			print("가속");
+		}
+		else
+		{
+			print("감속");
+		}
+
+		rigid.velocity = velocity;
+	}
+
+	private float CalculatedSpeed()
+	{
+		return maxSpeed;
+	}
+
+	public void Turn(float angle)
+	{
+		float rotY = transform.eulerAngles.y;
+		rotY = rotY > 180 ? rotY - 360f : rotY;
+		float sign = Mathf.Sign(angle - rotY);
+		float lerpAngle = rotY + sign * turnRate * Time.deltaTime;
+		if (sign != Mathf.Sign(angle - lerpAngle))
+		{
+			lerpAngle = angle;
+		}
+
+		transform.rotation = Quaternion.Euler(0, lerpAngle, 0);
+		this.angle = lerpAngle;
 	}
 }
---
+++ DriftHype/Assets/01_scripts/UI/HandleUI.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class HandleUI : MonoBehaviour
+{
+	[SerializeField] private CarController carController;
+	[Tooltip("Degree Per Second")]
+	[SerializeField] private float turnRate;
+	[Range(-180f, 0f)] 
+	[SerializeField] private float minAngle;
+	[Range(0f, 180f)]
+	[SerializeField] private float maxAngle;
+
+	private void Update()
+	{
+		if (Input.touchCount > 0)
+		{
+			Vector2 touchPos = Input.GetTouch(0).position;
+			Vector2 dir = touchPos - (Vector2)transform.position;
+
+			float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90;
+			angle = angle > 180f ? -360 + angle : angle;
+			angle = Mathf.Clamp(angle, minAngle, maxAngle);
+			float rotZ = transform.eulerAngles.z;
+			rotZ = rotZ > 180 ? rotZ - 360f : rotZ;
+			float sign = Mathf.Sign(angle - rotZ);
+
+			float lerpAngle = rotZ + sign * turnRate * Time.deltaTime;
+			if (sign != Mathf.Sign(angle - lerpAngle))
+			{
+				lerpAngle = angle;
+			}
+
+			transform.rotation = Quaternion.Euler(0, 0, lerpAngle);
+			carController.Turn(-lerpAngle);
+		}
+	}
+}
